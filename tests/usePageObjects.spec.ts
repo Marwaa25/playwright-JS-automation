3abf07c34b752523eab4e72c2f1b585624711c38
Marwa <siarmarwa@gmail.com>
2026-01-05 23:42:04 +0100

    argos ci

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -33,3 +33,8 @@ test('parametrized methods', async({page}) => {
     await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(5)
     await pm.onDatePickerPage().selectDatepickerWithRangeFromToday(6,15)
 })
+test.only('testing with argos ci', async({page}) => {
+    const pm = new PageManager(page)
+    await pm.navigateTo().formLayoutPage()
+    await pm.navigateTo().datepickerPage()
+})
